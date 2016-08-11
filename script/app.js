5482a650d4142d7e1fbd97c8e26c7b421dd4ad36
ildar <ildarcold@gmail.com>
2016-08-11 12:03:33 +0300

    Показывай фильтрацию контактов Запомни некоторые моменты, например, записывать как массив, точка с запятой ставить в некоторых местах или нет

--- script/app.js
+++ script/app.js
@@ -1,24 +1,28 @@
 var INFO = [
   {
-      id: 1,
+      id: 0,
+      img: 'http://downloadicons.net/sites/default/files/women-business-user-icon-44928.png',
       name: "Katya",
       surname: "Merci",
       tel: "89297220291"
   },
   {
-      id: 2,
+      id: 1,
       name: "Masha",
+      img: 'http://www.clker.com/cliparts/b/1/f/a/1195445301811339265dagobert83_female_user_icon.svg.med.png',
       surname: "Ledyava",
       tel: "89297220211"
   },
   {
-      id: 3,
+      id: 2,
       name: "Dinara",
+      img: 'http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/user-female-alt-icon.png',
       surname: "Sotnikova",
       tel: "89297220222"
   },
   {
-      id: 4,
+      id: 3,
+      img: 'https://cdn3.iconfinder.com/data/icons/rcons-user-action/32/boy-512.png',
       name: "Sasha",
       surname: "Sotnikov",
       tel: "89297220233"
@@ -28,23 +32,49 @@ var INFO = [
 var Contact = React.createClass({
     render: function() {
       return(
-        <li>
-          <div>{this.props.id}</div>
-          <div>{this.props.name}</div>
-          <div>{this.props.surname}</div>
+        <li className="contact_list">
+          <img src={this.props.img} />
+          <div className="contact_info">
+            <div className="name">{this.props.name}</div>
+            <div className="tel">{this.props.tel}</div>
+          </div>
         </li>
       );
     }
 });
 
 var ContactsList = React.createClass({
+    getInitialState: function () {
+      return{
+        displayedInfo: INFO
+      };
+    },
+
+    changeEvent: function (e){
+      var searchEvent = e.target.value.toLowerCase();
+      // console.log(searchEvent);
+      var displayedInfo = INFO.filter(function(el) {
+        var searchElement = el.name.toLowerCase();
+        return searchElement.indexOf(searchEvent) !== -1;
+      })
+      this.setState({
+        displayedInfo: displayedInfo
+      });
+    },
+
     render: function(){
       return (
-        <div>
+        <div className="container">
+          <input type="text" onChange={this.changeEvent}/>
           <ul>
               {
-                INFO.map(function (el) {
-                  return <Contact key={el.id} name={el.name} id={el.id} surname={el.surname}/>
+                this.state.displayedInfo.map(function (el) {
+                  return <Contact
+                          key={el.id}
+                          name={el.name}
+                          tel={el.tel}
+                          surname={el.surname}
+                          img={el.img}/>
                 })
               }
           </ul>
