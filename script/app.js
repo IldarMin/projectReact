1d58d960f499754e9d0955a03660eac7e8ecd62a
ildar <ildarcold@gmail.com>
2016-08-11 10:44:04 +0300

    Начало использования Reactjs Простой вывод данных с массива

--- script/app.js
+++ script/app.js
@@ -1,15 +1,59 @@
-var HelloWorld = React.createClass({
+var INFO = [
+  {
+      id: 1,
+      name: "Katya",
+      surname: "Merci",
+      tel: "89297220291"
+  },
+  {
+      id: 2,
+      name: "Masha",
+      surname: "Ledyava",
+      tel: "89297220211"
+  },
+  {
+      id: 3,
+      name: "Dinara",
+      surname: "Sotnikova",
+      tel: "89297220222"
+  },
+  {
+      id: 4,
+      name: "Sasha",
+      surname: "Sotnikov",
+      tel: "89297220233"
+  }
+];
+
+var Contact = React.createClass({
+    render: function() {
+      return(
+        <li>
+          <div>{this.props.id}</div>
+          <div>{this.props.name}</div>
+          <div>{this.props.surname}</div>
+        </li>
+      );
+    }
+});
+
+var ContactsList = React.createClass({
     render: function(){
-      return <h1>Hello {this.props.name}, I love {this.props.loves} !</h1>
+      return (
+        <div>
+          <ul>
+              {
+                INFO.map(function (el) {
+                  return <Contact key={el.id} name={el.name} id={el.id} surname={el.surname}/>
+                })
+              }
+          </ul>
+        </div>
+      );
     }
 });
 
 ReactDOM.render(
-  <div>
-    <HelloWorld name="Ildar" loves="football"/>
-    <HelloWorld name="Katya" loves="tennis"/>
-    <HelloWorld name="Masha" loves="volleyball"/>
-    <HelloWorld name="Manya" loves="hockey"/>
-  </div>,
-  document.getElementById('content')
+    <ContactsList />,
+    document.getElementById('content')
 );
